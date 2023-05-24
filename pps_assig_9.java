a6904e269b1e10abc0c69a95994c2aa6342a6262
Abhii202815 <132070408+Abhii202815@users.noreply.github.com>
2023-05-24 15:22:04 +0530

    Add files via upload

--- pps_assig_9.java
+++ pps_assig_9.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
-class leapyear {
+class leap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
 
-        System.out.println("Enter any year");
+        System.out.println("Enter the year");
 
         n = sc.nextInt();
 
-        if (n % 400 == 0 || (n % 4 == 0 && (n % 100 != 0))) {
+        if (n % 400 == 0 || (n % 4 == 0 && n % 100 != 0))
             System.out.println("Entered year is a leap year");
 
-        } else {
+        else
             System.out.println("Entered year is not a leap year");
-        }
     }
 }
